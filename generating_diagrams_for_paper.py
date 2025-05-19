4a287031dba7a555c3bdf67d32fc6f43b1757e28
Lui Yu Sen <luiyusen@uchicago.edu>
2025-05-19 17:05:21 -0500

    updated generating plots script

--- generating_diagrams_for_paper.py
+++ generating_diagrams_for_paper.py
@@ -8,6 +8,7 @@ from tqdm import tqdm
 import pickle
 import scipy.stats as st
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # folder management ===============================================================================
 folder_path = "diagrams_for_paper"
@@ -32,6 +33,19 @@ with open('name_lists.pkl', 'rb') as f:
     country_indices, action_indices, date_indices, database_indices = pickle.load(f)
 
 # plot heatmap ====================================================================================
-year = 2010
-action = 3
-database = database_indices[database_indices['database'] == 'GDELT'].iloc[0]
+year = 2016
+action = 3 # most common CAMEO code is 4, which is indexed as 3
+database = database_indices[database_indices['database'] == 'GDELT'].iloc[0]['index']
+number_of_countries = 20 # focus on top 100 actors
+
+data_slice = data[:, :, action, year - 2000, database].todense()
+
+ax = sns.heatmap(data_slice[:number_of_countries, :number_of_countries], linewidths=0, cmap='magma_r', xticklabels=False)
+ax.set_yticks(np.arange(number_of_countries))
+ax.set_yticklabels(country_indices['country'][:number_of_countries], fontsize=6, rotation=0)
+ax.set_title(f"Top {number_of_countries} actors for {year} and CAMEO action {action_indices['action'].iloc[action]}")
+plt.savefig(os.path.join(folder_path, 'heatmap.png'),
+            bbox_inches='tight',
+            dpi=600)
+
+# overdispersion plot =============================================================================
