51f0367ec237752dfeaf6c69718fbc10fd45a6ff
Lui Yu Sen <luiyusen@uchicago.edu>
2025-08-08 21:51:31 +0800

    some changes to testing.py

--- testing.py
+++ testing.py
@@ -10,7 +10,8 @@ def restart_kernel():
 restart_kernel()
 
 import bptf
-from bptf import BPTF
+# from bptf import BPTF
+from own_implementation import BPTF
 import numpy as np
 import pandas as pd
 import sparse
@@ -37,23 +38,34 @@ assert os.path.exists('sptensor.pkl'), 'No such file.'
 with open('sptensor.pkl', 'rb') as f:
     Y = pickle.load(f)
 
+device = 'cuda'
+
 Y = Y.todense()
 Y = Y[:, :, :, :, 0]
 
-I_range = np.arange(Y.shape[0])
-A_range = np.arange(Y.shape[2])
-T_range = np.arange(Y.shape[3])
+# mask
+mask = np.ones(Y.shape)
+diagonal_indices = np.arange(0, Y.shape[0])
+mask[diagonal_indices, diagonal_indices, :, :] = 0
+mask = mask.astype(np.int64)
+mask = torch.tensor(mask, dtype=torch.float64, device=device)
 
-I, A, T = np.meshgrid(I_range, A_range, T_range, indexing='ij')
-I_flattened = np.ravel(I)
-A_flattened = np.ravel(A)
-T_flattened = np.ravel(T)
+# I_range = np.arange(Y.shape[0])
+# A_range = np.arange(Y.shape[2])
+# T_range = np.arange(Y.shape[3])
 
-mask_coords = np.vstack([I_flattened, I_flattened, A_flattened, T_flattened])
-mask = sparse.COO(coords=mask_coords, data=np.ones(shape = mask_coords.shape[1]), shape=Y.shape)
+# I, A, T = np.meshgrid(I_range, A_range, T_range, indexing='ij')
+# I_flattened = np.ravel(I)
+# A_flattened = np.ravel(A)
+# T_flattened = np.ravel(T)
+
+# mask_coords = np.vstack([I_flattened, I_flattened, A_flattened, T_flattened])
+# mask = sparse.COO(coords=mask_coords, data=np.ones(shape = mask_coords.shape[1]), shape=Y.shape)
+
+Y = torch.tensor(Y, dtype=torch.float64, device=device)
 
 model = BPTF(data_shape=Y.shape, n_components=100)
-model.fit(Y, max_iter = 50, mask=mask,verbose=True)
+model.fit(Y, max_iter = 50, mask=mask,verbose=True, tol=1e-8)
 
 with open('bptf_icews.pkl', 'wb') as f:
     pickle.dump(model, f)
